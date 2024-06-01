76c1aaa67c1ce2f45243119d6b8399e2907a017e
MOHAMED BAKULY <119895383+MOHAMEDBAKULY@users.noreply.github.com>
2024-06-01 09:35:26 +0300

    Ratings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const messages = [
+  "React is really fun 😄.",
+  "Invest your new income 🤑",
+  "Learning React opens opportunities 🚀.",
+  "Learn React ⚛️",
+  "React skills enhance employability 💼.",
+  "Future jobs need React 🔮.",
+  "Apply for jobs 💼",
+  "React library is excellent 🌟",
+];
+
+export default function App() {
+  const [msg, setMsg] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+  // const [test, setTest] = useState({ name: "Bakuly" });
+
+  const handleForward = () => {
+    if (msg > 1) setMsg((m) => m - 1);
+  };
+  const handleBackward = () => {
+    if (msg < 8) setMsg((m) => m + 1);
+    // setTest({ name: "Rashid" });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={() => setIsOpen(!isOpen)} className="close">
+        &times;
+      </button>
+
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <span className={msg >= 1 ? "active" : ""}>1</span>
+            <span className={msg >= 2 ? "active" : ""}>2</span>
+            <span className={msg >= 3 ? "active" : ""}>3</span>
+            <span className={msg >= 4 ? "active" : ""}>4</span>
+            <span className={msg >= 5 ? "active" : ""}>5</span>
+            <span className={msg >= 6 ? "active" : ""}>6</span>
+            <span className={msg >= 7 ? "active" : ""}>7</span>
+            <span className={msg >= 8 ? "active" : ""}>8</span>
+          </div>
+
+          <div className="message">
+            {" "}
+            Steps {msg}: {messages[msg - 1]}
+            {/* {test.name} */}
+          </div>
+
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#55ffee", color: "#000" }}
+              onClick={handleForward}
+            >
+              Backward
+            </button>
+            <button
+              style={{ backgroundColor: "#55ffee", color: "#000" }}
+              onClick={handleBackward}
+            >
+              forward
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
-
-export default App;
